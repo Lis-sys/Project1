eaed18c12fabdffae4d34ad8a2479e6c8dc5a1c6
Lis-sys <63111606+Lis-sys@users.noreply.github.com>
2024-06-25 17:41:31 +0500

    Add files via upload

---
+++ src/config/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -0,0 +1,110 @@
+package config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.sql.DataSource;
+import java.util.EnumSet;
+
+
+public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerCharacterEncodingFilter(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
+    }
+
+    private void registerCharacterEncodingFilter(ServletContext aContext) {
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        FilterRegistration.Dynamic characterEncoding = aContext.addFilter("characterEncoding", characterEncodingFilter);
+        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
+    }
+
+    @Configuration
+    @ComponentScan("ru.arsen.springcourse")
+    @EnableWebMvc
+    public static class SpringConfig implements WebMvcConfigurer {
+
+        private final ApplicationContext applicationContext;
+
+        @Autowired
+        public SpringConfig(ApplicationContext applicationContext) {
+            this.applicationContext = applicationContext;
+        }
+
+        @Bean
+        public SpringResourceTemplateResolver templateResolver() {
+            SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+            templateResolver.setApplicationContext(applicationContext);
+            templateResolver.setPrefix("/WEB-INF/views/");
+            templateResolver.setSuffix(".html");
+            templateResolver.setCharacterEncoding("UTF-8");
+            return templateResolver;
+        }
+
+        @Bean
+        public SpringTemplateEngine templateEngine() {
+            SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+            templateEngine.setTemplateResolver(templateResolver());
+            templateEngine.setEnableSpringELCompiler(true);
+            return templateEngine;
+        }
+
+        @Override
+        public void configureViewResolvers(ViewResolverRegistry registry) {
+            ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+            resolver.setTemplateEngine(templateEngine());
+            resolver.setCharacterEncoding("UTF-8");
+
+            registry.viewResolver(resolver);
+        }
+
+        @Bean
+        public DataSource dataSource() {
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+            dataSource.setDriverClassName("org.postgresql.Driver");
+            dataSource.setUrl("jdbc:postgresql://localhost:5432/project1");
+            dataSource.setUsername("postgres");
+            dataSource.setPassword("postgres");
+
+            return dataSource;
+        }
+
+        @Bean
+        public JdbcTemplate jdbcTemplate() {
+            return new JdbcTemplate(dataSource());
+        }
+    }
+}
